f7d123cd7a2fdf81b000406a68a2f57b037ccedb
Tom <tomwells3@gmail.com>
2021-08-03 15:21:06 +0100

    delete fuicntion

--- dfa-tool/src/components/QuizTitles.jsx
+++ dfa-tool/src/components/QuizTitles.jsx
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useGlobalContext } from "./TestContext";
 import { AiOutlineQuestionCircle } from "react-icons/ai";
+import { IoRemoveCircleOutline } from "react-icons/io5";
+import { RiDeleteBinLine } from "react-icons/ri";
+import { ImCross } from "react-icons/im";
 import Loading from "./Loading";
+import axios from "axios";
 
 function QuizTitle() {
   const { openModal, setLoading, isLoading } = useGlobalContext();
@@ -35,6 +39,15 @@ function QuizTitle() {
   // this will run until the data is fetched
   if (isLoading) {
     return <Loading />;
+  }
+
+  function handleRemove(id) {
+    const url = "https://dfa-quiz.herokuapp.com/delete/";
+    const url2 = `${url}${id}`;
+    console.log(" the url is " + url2);
+    axios.delete("https://dfa-quiz.herokuapp.com/delete/" + id);
+    // axios.delete("http://localhost:5000/delete/" + id);
+    window.location.reload();
   }
 
   return (
@@ -72,12 +85,18 @@ function QuizTitle() {
               return (
                 <div className="qtitlecontainer" key={index}>
                   <h3>{quiz.quiztitle}</h3>
-                  <Link
-                    to={`/SingleQuestion/${quiz._id}`}
-                    className="quizbutton"
-                  >
-                    Open Quiz
-                  </Link>
+                  <div className="test">
+                    <Link
+                      to={`/SingleQuestion/${quiz._id}`}
+                      className="quizbutton"
+                    >
+                      Open Quiz
+                    </Link>
+                    <ImCross
+                      className="removeQuizBtn"
+                      onClick={() => handleRemove(quiz._id)}
+                    ></ImCross>
+                  </div>
                 </div>
               );
             }
